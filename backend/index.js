4f1ff845c6b63c516742c4b5e496406f8a94af78
Pratyush <pratyushjain1916@gmail.com>
2024-04-29 10:35:22 +0530

    Completed backend

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express, { request, response } from "express";
+import { PORT } from "./config.js";
+import { mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js"
+
+const app = express();
+
+//Middleware for parsing request body
+app.use(express.json());
+
+
+//Middleware for handling CORS POLICY
+//Option 1: Allow All origins with default of cors(*)
+// app.use(cors());
+
+//Option 2: Allow Custom origins
+app.use(cors({
+    origin: 'https://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));
+
+app.get('/', ( request, response )=>{
+    console.log(request);
+    return response.status(234).send('welcome to mern stack tutorial')
+});
+
+app.use('/books', booksRoute); // here '/books' means add this prefix to booksRoute
+
+mongoose
+    .connect(mongoDBURL)
+        .then(()=>{
+            console.log("App is connected to database");
+            app.listen(PORT, () => {
+                console.log(`App is listening to port ${PORT}`);
+            });
+    })
+        .catch((error)=>{
+            console.log();
+    })
